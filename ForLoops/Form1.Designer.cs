816f48ca84e019f559e68492089e08560b6e67f0
Tony Theodoropoulos <theodot@outlook.com>
2020-12-09 17:20:54 -0500

    Add project files.

---
+++ ForLoops/Form1.Designer.cs
@@ -0,0 +1,272 @@
+﻿
+namespace ForLoops
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.titleLabel = new System.Windows.Forms.Label();
+            this.q1Button = new System.Windows.Forms.Button();
+            this.q1Output = new System.Windows.Forms.Label();
+            this.q2Output = new System.Windows.Forms.Label();
+            this.q2Button = new System.Windows.Forms.Button();
+            this.q3Output = new System.Windows.Forms.Label();
+            this.q3Button = new System.Windows.Forms.Button();
+            this.q4Output = new System.Windows.Forms.Label();
+            this.q4Button = new System.Windows.Forms.Button();
+            this.q5Output = new System.Windows.Forms.Label();
+            this.q5Button = new System.Windows.Forms.Button();
+            this.q6Output = new System.Windows.Forms.Label();
+            this.q6Button = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // titleLabel
+            // 
+            this.titleLabel.BackColor = System.Drawing.Color.DarkGoldenrod;
+            this.titleLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.titleLabel.ForeColor = System.Drawing.Color.White;
+            this.titleLabel.Location = new System.Drawing.Point(-1, -4);
+            this.titleLabel.Name = "titleLabel";
+            this.titleLabel.Size = new System.Drawing.Size(381, 64);
+            this.titleLabel.TabIndex = 23;
+            this.titleLabel.Text = "for Loops";
+            this.titleLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // q1Button
+            // 
+            this.q1Button.BackColor = System.Drawing.Color.DimGray;
+            this.q1Button.FlatAppearance.MouseOverBackColor = System.Drawing.Color.DarkGoldenrod;
+            this.q1Button.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.q1Button.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.q1Button.ForeColor = System.Drawing.Color.White;
+            this.q1Button.Location = new System.Drawing.Point(20, 86);
+            this.q1Button.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.q1Button.Name = "q1Button";
+            this.q1Button.Size = new System.Drawing.Size(156, 44);
+            this.q1Button.TabIndex = 24;
+            this.q1Button.Text = "question 1";
+            this.q1Button.UseVisualStyleBackColor = false;
+            this.q1Button.Click += new System.EventHandler(this.q1Button_Click);
+            // 
+            // q1Output
+            // 
+            this.q1Output.BackColor = System.Drawing.Color.White;
+            this.q1Output.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.q1Output.ForeColor = System.Drawing.Color.Black;
+            this.q1Output.Location = new System.Drawing.Point(20, 134);
+            this.q1Output.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.q1Output.Name = "q1Output";
+            this.q1Output.Size = new System.Drawing.Size(156, 42);
+            this.q1Output.TabIndex = 25;
+            this.q1Output.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // q2Output
+            // 
+            this.q2Output.BackColor = System.Drawing.Color.White;
+            this.q2Output.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.q2Output.ForeColor = System.Drawing.Color.Black;
+            this.q2Output.Location = new System.Drawing.Point(20, 243);
+            this.q2Output.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.q2Output.Name = "q2Output";
+            this.q2Output.Size = new System.Drawing.Size(156, 42);
+            this.q2Output.TabIndex = 27;
+            this.q2Output.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // q2Button
+            // 
+            this.q2Button.BackColor = System.Drawing.Color.DimGray;
+            this.q2Button.FlatAppearance.MouseOverBackColor = System.Drawing.Color.DarkGoldenrod;
+            this.q2Button.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.q2Button.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.q2Button.ForeColor = System.Drawing.Color.White;
+            this.q2Button.Location = new System.Drawing.Point(20, 195);
+            this.q2Button.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.q2Button.Name = "q2Button";
+            this.q2Button.Size = new System.Drawing.Size(156, 44);
+            this.q2Button.TabIndex = 26;
+            this.q2Button.Text = "question 2";
+            this.q2Button.UseVisualStyleBackColor = false;
+            this.q2Button.Click += new System.EventHandler(this.q2Button_Click);
+            // 
+            // q3Output
+            // 
+            this.q3Output.BackColor = System.Drawing.Color.White;
+            this.q3Output.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.q3Output.ForeColor = System.Drawing.Color.Black;
+            this.q3Output.Location = new System.Drawing.Point(20, 359);
+            this.q3Output.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.q3Output.Name = "q3Output";
+            this.q3Output.Size = new System.Drawing.Size(156, 42);
+            this.q3Output.TabIndex = 29;
+            this.q3Output.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // q3Button
+            // 
+            this.q3Button.BackColor = System.Drawing.Color.DimGray;
+            this.q3Button.FlatAppearance.MouseOverBackColor = System.Drawing.Color.DarkGoldenrod;
+            this.q3Button.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.q3Button.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.q3Button.ForeColor = System.Drawing.Color.White;
+            this.q3Button.Location = new System.Drawing.Point(20, 311);
+            this.q3Button.Margin = new System.Windows.Forms.Padding(4);
+            this.q3Button.Name = "q3Button";
+            this.q3Button.Size = new System.Drawing.Size(156, 44);
+            this.q3Button.TabIndex = 28;
+            this.q3Button.Text = "question 3";
+            this.q3Button.UseVisualStyleBackColor = false;
+            this.q3Button.Click += new System.EventHandler(this.q3Button_Click);
+            // 
+            // q4Output
+            // 
+            this.q4Output.BackColor = System.Drawing.Color.White;
+            this.q4Output.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.q4Output.ForeColor = System.Drawing.Color.Black;
+            this.q4Output.Location = new System.Drawing.Point(20, 475);
+            this.q4Output.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.q4Output.Name = "q4Output";
+            this.q4Output.Size = new System.Drawing.Size(156, 42);
+            this.q4Output.TabIndex = 31;
+            this.q4Output.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // q4Button
+            // 
+            this.q4Button.BackColor = System.Drawing.Color.DimGray;
+            this.q4Button.FlatAppearance.MouseOverBackColor = System.Drawing.Color.DarkGoldenrod;
+            this.q4Button.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.q4Button.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.q4Button.ForeColor = System.Drawing.Color.White;
+            this.q4Button.Location = new System.Drawing.Point(20, 427);
+            this.q4Button.Margin = new System.Windows.Forms.Padding(4);
+            this.q4Button.Name = "q4Button";
+            this.q4Button.Size = new System.Drawing.Size(156, 44);
+            this.q4Button.TabIndex = 30;
+            this.q4Button.Text = "question 4";
+            this.q4Button.UseVisualStyleBackColor = false;
+            this.q4Button.Click += new System.EventHandler(this.q4Button_Click);
+            // 
+            // q5Output
+            // 
+            this.q5Output.BackColor = System.Drawing.Color.White;
+            this.q5Output.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.q5Output.ForeColor = System.Drawing.Color.Black;
+            this.q5Output.Location = new System.Drawing.Point(201, 134);
+            this.q5Output.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.q5Output.Name = "q5Output";
+            this.q5Output.Size = new System.Drawing.Size(156, 88);
+            this.q5Output.TabIndex = 33;
+            this.q5Output.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // q5Button
+            // 
+            this.q5Button.BackColor = System.Drawing.Color.DimGray;
+            this.q5Button.FlatAppearance.MouseOverBackColor = System.Drawing.Color.DarkGoldenrod;
+            this.q5Button.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.q5Button.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.q5Button.ForeColor = System.Drawing.Color.White;
+            this.q5Button.Location = new System.Drawing.Point(201, 86);
+            this.q5Button.Margin = new System.Windows.Forms.Padding(4);
+            this.q5Button.Name = "q5Button";
+            this.q5Button.Size = new System.Drawing.Size(156, 44);
+            this.q5Button.TabIndex = 32;
+            this.q5Button.Text = "question 5";
+            this.q5Button.UseVisualStyleBackColor = false;
+            this.q5Button.Click += new System.EventHandler(this.q5Button_Click);
+            // 
+            // q6Output
+            // 
+            this.q6Output.BackColor = System.Drawing.Color.White;
+            this.q6Output.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.q6Output.ForeColor = System.Drawing.Color.Black;
+            this.q6Output.Location = new System.Drawing.Point(201, 289);
+            this.q6Output.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.q6Output.Name = "q6Output";
+            this.q6Output.Size = new System.Drawing.Size(156, 133);
+            this.q6Output.TabIndex = 35;
+            this.q6Output.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // q6Button
+            // 
+            this.q6Button.BackColor = System.Drawing.Color.DimGray;
+            this.q6Button.FlatAppearance.MouseOverBackColor = System.Drawing.Color.DarkGoldenrod;
+            this.q6Button.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.q6Button.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.q6Button.ForeColor = System.Drawing.Color.White;
+            this.q6Button.Location = new System.Drawing.Point(201, 241);
+            this.q6Button.Margin = new System.Windows.Forms.Padding(4);
+            this.q6Button.Name = "q6Button";
+            this.q6Button.Size = new System.Drawing.Size(156, 44);
+            this.q6Button.TabIndex = 34;
+            this.q6Button.Text = "question 6";
+            this.q6Button.UseVisualStyleBackColor = false;
+            this.q6Button.Click += new System.EventHandler(this.q6Button_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Black;
+            this.ClientSize = new System.Drawing.Size(377, 546);
+            this.Controls.Add(this.q6Output);
+            this.Controls.Add(this.q6Button);
+            this.Controls.Add(this.q5Output);
+            this.Controls.Add(this.q5Button);
+            this.Controls.Add(this.q4Output);
+            this.Controls.Add(this.q4Button);
+            this.Controls.Add(this.q3Output);
+            this.Controls.Add(this.q3Button);
+            this.Controls.Add(this.q2Output);
+            this.Controls.Add(this.q2Button);
+            this.Controls.Add(this.q1Output);
+            this.Controls.Add(this.q1Button);
+            this.Controls.Add(this.titleLabel);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Name = "Form1";
+            this.Text = "for Loops";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label titleLabel;
+        private System.Windows.Forms.Button q1Button;
+        private System.Windows.Forms.Label q1Output;
+        private System.Windows.Forms.Label q2Output;
+        private System.Windows.Forms.Button q2Button;
+        private System.Windows.Forms.Label q3Output;
+        private System.Windows.Forms.Button q3Button;
+        private System.Windows.Forms.Label q4Output;
+        private System.Windows.Forms.Button q4Button;
+        private System.Windows.Forms.Label q5Output;
+        private System.Windows.Forms.Button q5Button;
+        private System.Windows.Forms.Label q6Output;
+        private System.Windows.Forms.Button q6Button;
+    }
+}
+
---
+++ ForLoops/Form1.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ForLoops
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void q1Button_Click(object sender, EventArgs e)
+        {
+            //1.  Create a loop that displays all values inclusive between 1-5.
+
+            for (int i = 1; i <= 5; i++)
+            {
+                
+            }
+        }
+
+        private void q2Button_Click(object sender, EventArgs e)
+        {
+            //2.  Create a loop that displays all values inclusive between 10-6.
+
+
+        }
+
+        private void q3Button_Click(object sender, EventArgs e)
+        {
+            //3.  Create a loop that displays even numbers inclusive between 2 and 10
+
+
+        }
+
+        private void q4Button_Click(object sender, EventArgs e)
+        {
+            //4.  Create a loop that displays the sum of all numbers inclusive 
+            //    between 1 and 10. The answers should be 55.
+
+
+        }
+
+        private void q5Button_Click(object sender, EventArgs e)
+        {
+            //5. Create a loop that displays all leap years inclusive between 
+            //   1990 and 2010. The output should be: 1992 1998 2000 2004 2008
+            //
+            //
+
+
+        }
+
+        private void q6Button_Click(object sender, EventArgs e)
+        {
+            //6. Print the following patterns using a loop:
+            //
+            //   *
+            //   **
+            //   ***
+            //   ****
+            //   *****
+            //
+            //   Hint: Start off with a string that has 1 "*" in it, then 
+            //   in the loop you will add another "*"
+
+
+        }
+    }
+}
