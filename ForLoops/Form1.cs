7b0a32f57772e29c854aaea6106818bb99381d4d
P4 <umeaansa555@ed.amdsb.ca>
2021-11-10 13:14:56 -0500

    q4 inc. q5 done

--- ForLoops/Form1.cs
+++ ForLoops/Form1.cs
@@ -23,24 +23,41 @@ namespace ForLoops
 
             for (int i = 1; i <= 5; i++)
             {
-                
+                q1Output.Text += $"{i} ";
             }
 
 
             ///2.  Create a loop that displays all values inclusive between 10-6.
 
+            for (int i = 10; i >=6; i--)
+            {
+                q2Output.Text += $"{i} ";
+            }
 
             ///3.  Create a loop that displays even numbers inclusive between 2 and 10
 
+            for (int i = 2; i <= 10; i = i + 2)
+            {
+                q3Output.Text += $"{i} ";
+            }
 
             ///4.  Create a loop that displays the sum of all numbers inclusive 
             ///between 1 and 10. The answers should be 55.
+            
+            int sum = 1;
+            for (int i = 1; i <= 10; sum = i++ + i)
+            {
+                q4Output.Text += $"{sum} ";
+            }
 
 
             ///5. Create a loop that displays all leap years inclusive between 
-            ///1990 and 2010. The output should be: 1992 1998 2000 2004 2008
+            ///1990 and 2010. The output should be: 1992 1996 2000 2004 2008
 
-
+            for (int i = 1990 +2 ; i <= 2010; i = i + 4)
+            {
+                q5Output.Text += $"{i} ";
+            }
 
             ///6. Print the following patterns using a loop:
             ///
